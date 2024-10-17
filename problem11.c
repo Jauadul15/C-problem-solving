3b5d07aa0633aa023acb4be9fda3d8e71de12bf4
Jauadul15 <rashik.gmh@gmail.com>
2024-10-17 18:19:53 +0600

    summation of an array with abs

---
+++ problem11.c
@@ -0,0 +1,17 @@
+    #include<stdio.h>
+    #include<stdlib.h>
+
+    int main(){
+        int n;
+        scanf("%d",&n);
+        int a[n];
+        long long sum=0;
+
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d",&a[i]);
+            sum +=a[i];
+        }
+        printf("%lld\n", llabs(sum));
+        return 0;
+    }
