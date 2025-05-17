efbfb37a95410fc897160a88c742997760d3d50f
Jauadul15 <rashik.gmh@gmail.com>
2025-05-17 11:24:05 +0600

    new

---
+++ conditional statement recap problem solve/problem2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %c %d",&a,&c,&b);
+    if (c=='>'&& a>b)
+    {
+        printf("Right\n");
+        
+    }else if(c=='<'&& a<b){
+        printf("right\n");
+    }else if(c=='='&& a==b){
+        printf("Right\n");
+    }else{
+        printf("Wrong\n");
+    }
+    
+    return 0;
+}
